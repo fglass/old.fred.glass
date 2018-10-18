8b8fac9914ae266f5cfe8781c69281c8836b853b
Fred Glass <glasshcs@gmail.com>
2018-10-18 17:48:31 +0100

    V1.1

--- js/head.js
+++ js/head.js
@@ -64,7 +64,8 @@ var head = function() {
 
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
-        renderer.setSize(window.innerWidth, window.innerHeight);
+        var navOffset = 102;
+        renderer.setSize(window.innerWidth, window.innerHeight - navOffset);
     }
 
     function onDocumentMouseMove(event) {
